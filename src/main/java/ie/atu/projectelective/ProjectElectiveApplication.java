980760d6c1e6955910eb5313c81f97f831150dc9
Johnson Shogbaike <g00389668@atu.ie>
2024-01-12 19:35:17 +0000

    elective microservice made

---
+++ src/main/java/ie/atu/projectelective/ProjectElectiveApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.projectelective;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectElectiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectElectiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/projectelective/ProjectElectiveApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.projectelective;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectElectiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
